ef90e1f672ca1aefacae7e392f3992fcb42e1221
Ali Samadi <alisamadi.096@gmail.com>
2025-03-10 10:11:28 -0500

    ShowSelection.jsx is done

--- src/shows/ShowSelection.jsx
+++ src/shows/ShowSelection.jsx
@@ -1,6 +1,19 @@
+
 import "./shows.css";
 
 /** A navbar that allows users to choose between a list of shows */
-export default function ShowSelection() {
-  return <nav className="shows"></nav>;
+export default function ShowSelection({ showsList, setSelectedShow }) {
+  return (
+    <nav className="shows">
+      {showsList.map((show) => (
+        <a
+          key={show.name}
+          className={"show"}
+          onClick={() => setSelectedShow(show)}
+        >
+          {show.name}
+        </a>
+      ))}
+    </nav>
+  );
 }
